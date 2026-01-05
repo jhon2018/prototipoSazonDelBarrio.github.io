bf5d3a8988e723cc6113829baf8b693f703db472
JonathanSise <jhonvs93@hotmail.com>
2026-01-04 20:22:45 -0500

    Listo para Render

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/prototipoSazonDelBarrio.github.io/', // IMPORTANTE: nombre de tu repo
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  }
+  base: '/', // IMPORTANTE: así para Render
 })
