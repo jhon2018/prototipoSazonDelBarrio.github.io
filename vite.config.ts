d1293157d2caeefe4e3a3a830daa3858c1e7a42f
JonathanSise <jhonvs93@hotmail.com>
2026-01-04 19:46:12 -0500

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/prototipoSazonDelBarrio.github.io/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/prototipoSazonDelBarrio.github.io/', // IMPORTANTE: nombre de tu repo
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
 })
